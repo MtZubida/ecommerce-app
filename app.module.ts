6d6a6dbb0b5cf7e1691338312449a35aef77b316
MtZubida <100330350+MtZubida@users.noreply.github.com>
2023-05-12 21:11:06 +0600

    Add files via upload

--- app.module.ts
+++ app.module.ts
@@ -5,19 +5,28 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminModule } from './admin/adminmodule.module';
 import { SellerModule } from './sellerfile/seller.module';
 import { ModeratorModule } from './moderatorfile/moderator.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [AdminModule , SellerModule,ModeratorModule, TypeOrmModule.forRoot(
     { type: 'postgres',
-     host: 'localhost',
-     port: 5432,
+     host: 'containers-us-west-44.railway.app',
+     port: 6546,
      username: 'postgres',
-     password: 'root',
-     database: 'abc_ecommerce',
+     password: 'l5O8NBeoep5uWNxhsdFp',
+     database: 'railway',
      autoLoadEntities: true,
      synchronize: true,
    }
-   ),],
+   ),
+
+ ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', '../public'), // added ../ to get one folder back
+    serveRoot: '/public/' //last slash was important
+  }),
+
+],
   controllers: [AppController],
   providers: [AppService],
 })
