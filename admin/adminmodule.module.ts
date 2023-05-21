96d55efa9c9b3dde22fa0a0a41ccc8d25cddc982
MtZubida <100330350+MtZubida@users.noreply.github.com>
2023-05-21 08:49:13 +0600

    Update adminmodule.module.ts

--- admin/adminmodule.module.ts
+++ admin/adminmodule.module.ts
@@ -20,8 +20,8 @@ import { MailerModule } from "@nestjs-modules/mailer";
                ignoreTLS: true,
                secure: true,
                auth: {
-                   user: 'zubaida.gttc@gmail.com',
-                   pass: 'rljvkkbnbbizurof'
+                   user: '',
+                   pass: ''
                },
               }
   }),
